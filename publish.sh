4f5b792231908e319af9c6ef72791e23e773a597
kim jihoon <whitexozu@gmail.com>
2020-02-26 06:45:46 +0900

    ...

--- publish.sh
+++ publish.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+git --git-dir .git --work-tree=./ status
+git --git-dir .git --work-tree=./ add .
+git --git-dir .git --work-tree=./ commit -m "maybe add posts file"
+git --git-dir .git --work-tree=./ push origin master
+
 rm -rf ../whitexozu.github.io/*
 cp -r _site/* ../whitexozu.github.io/
+
 # git --git-dir ../whitexozu.github.io/.git branch
 git --git-dir ../whitexozu.github.io/.git --work-tree=../whitexozu.github.io status
 git --git-dir ../whitexozu.github.io/.git --work-tree=../whitexozu.github.io add .
